07a33b4cf5651dbd576020c3f6434cfd633273e0
Putra <utaputranto@gmail.com>
2026-07-20 01:07:12 +0700

    chore: integrate Firebase and configure CD workflow for App Distribution

---
+++ build-logic/convention/src/main/kotlin/AndroidFirebaseConventionPlugin.kt
@@ -0,0 +1,19 @@
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.dependencies
+
+class AndroidFirebaseConventionPlugin : Plugin<Project> {
+    override fun apply(target: Project) {
+        with(target) {
+            pluginManager.apply("com.google.gms.google-services")
+
+            dependencies {
+                val bom = libsExtension.findLibrary("firebase-bom").get()
+                val analytics = libsExtension.findLibrary("firebase-analytics").get()
+
+                "implementation"(platform(bom))
+                "implementation"(analytics)
+            }
+        }
+    }
+}

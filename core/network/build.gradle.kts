6280aff8345b3b1566fd6a0bddd0eb24adaa8bcd
Putra <utaputranto@gmail.com>
2026-07-19 23:00:07 +0700

    fix(network): check local.properties existence before setting task property

--- core/network/build.gradle.kts
+++ core/network/build.gradle.kts
@@ -48,7 +48,10 @@ val generateBuildConfig =
     tasks.register<GenerateBuildConfigTask>("generateBuildConfig") {
         outputDir.set(layout.buildDirectory.dir("generated/source/buildConfig/commonMain/kotlin"))
         tmdbToken.set(providers.environmentVariable("TMDB_TOKEN").orElse(""))
-        localPropertiesFile.set(project.rootProject.layout.projectDirectory.file("local.properties"))
+        val localProperties = project.rootProject.layout.projectDirectory.file("local.properties")
+        if (localProperties.asFile.exists()) {
+            localPropertiesFile.set(localProperties)
+        }
     }
 
 kotlin {
